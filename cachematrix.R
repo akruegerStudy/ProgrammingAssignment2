e04ec26bf793e0d9d15210d2afb247f1eada9346
akruegerStudy <study@andreaskrueger.de>
2014-06-20 05:50:01 +0200

    cleaned up comments

--- cachematrix.R
+++ cachematrix.R
@@ -1,58 +1,60 @@
 
 ## Programming Assignment 2
-##  Johns Hopkins - R Programming
+##  R Programming - Johns Hopkins
 ##  by Roger D. Peng, PhD, Jeff Leek, PhD, Brian Caffo, PhD
 ##
-## This solution is by Andreas Krueger 
+## This solution is by Andreas Krueger 20/6/2014
 ## 
 ## Goal:
 ## A matrix inversion is calculated, and then cached.  
 ## To save computation time, a repeated call of cacheSolve 
 ## does not recalculate - but returns the previous result. 
 ## 
-## N.B.: Contains not only the answer, but two functions to test them.
+## N.B.: Contains the answer, plus 2 functions to test it, at the bottom.
+##
 
-
-## makeCacheMatrix: 
-## This function creates a special "matrix" object that caches its inverse.
 
 makeCacheMatrix <- function(x = matrix()) {
-        ## creates a special matrix object which can cache its inverse
+        ## creates a special matrix object 
+        ## which can cache its inverse
         
-        inverse <- NULL
-        set <- function(y) {
+        inverse <- NULL                            # init with no inverse
+        
+        set <- function(y) {                       # (re)sets matrix
                 x <<- y
                 inverse <<- NULL
         }
-        get <- function() x
-        setinverse <- function(i) inverse <<- i
-        getinverse <- function() inverse
-        list(set = set, get = get,
+        
+        get <- function() x                        # returns matrix
+        setinverse <- function(i) inverse <<- i    # caches inverse 
+        getinverse <- function() inverse           # returns inverse
+        
+        list(set = set, get = get,                 # special object 4 functions
              setinverse = setinverse,
              getinverse = getinverse)
 }
         
 
-## cacheSolve: 
-## This function computes the inverse of the special "matrix" 
-## created by makeCacheMatrix above. 
-## If the inverse has already been calculated, then cacheSolve 
-## retrieves the inverse from the cache instead of recalculating it.
-
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+        ## Return the inverse of the matrix stored in 'x'
         ## if possible from previously cached inversion.
         
-        inverse <- x$getinverse()
+        inverse <- x$getinverse()                 # look for cached inverse
+        
         if(!is.null(inverse)) {
                 message("getting cached data")
-                return(inverse)
+                return(inverse)                   # return if exists already
         }
-        M <- x$get()
-        inverse <- solve(M, ...)
-        x$setinverse(inverse)
-        inverse
+                                                  # else:
+        
+        M <- x$get()                              # get the matrix
+        inverse <- solve(M, ...)                  # calculate the inverse
+        
+        x$setinverse(inverse)                     # cache result
+        inverse                                   # return result
 }                
+
+
 
 ################## Programming Assignment ends here ###############
 ##
@@ -61,30 +63,30 @@ cacheSolve <- function(x, ...) {
 ####### You can ignore the following in your evaluation  ##########
 
 
-myTest1 <- function (){
-        ## generates a matrix, asks for the inverse 
+myTest1 <- function (r=3){
+        ## generates a square matrix with r rows,
+        ## asks for the inverse, 
         ## and then for the inverse again
         
-        r <- 3   # number of rows of square matrix        
-        M   <- matrix (rnorm(r^2),r,r) # random matrix = invertible
-        CM  <- makeCacheMatrix(M)      # create our "special matrix object"
-        
-        Mi <- cacheSolve(CM)           # first call
+        # "special matrix object" from random matrix ( = probably invertible):
+        CM  <- makeCacheMatrix(  matrix (rnorm(r^2),r,r)  )
         
         # tests the result by multiplying the matrix with its inverse:
-        multiplyPrint <- function (M, Mi) print (round (  M %*% Mi ,5)) 
-        multiplyPrint(M, Mi)
+        multiplyPrint <- function (M1, M2) print (round ( M1 %*% M2 , 1)) 
+        
+        Mi <- cacheSolve(CM)           # 1st call
+        multiplyPrint( CM$get(), Mi )
         
         Mi <- cacheSolve(CM)           # 2nd call
-        multiplyPrint(M, Mi)
+        multiplyPrint( CM$get(), Mi )
 }
 
 myTest2 <- function (size = 1000){
         ## generates a HUGE matrix, asks for the inverse 
         ## and then for the inverse again. Timing measured.
         
-        M   <- matrix (rnorm(size^2), size, size)  # random matrix
-        CM  <- makeCacheMatrix(M)      # create our "special matrix"
+        # "special matrix object" from random matrix (=probably invertible):
+        CM  <- makeCacheMatrix(  matrix (rnorm(size^2),size,size)  )
         
         print(system.time(    Mi <- cacheSolve(CM)       ))    # 1st call
         print(system.time(    Mi <- cacheSolve(CM)       ))    # 2nd call
